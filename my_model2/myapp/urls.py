80d8f293a0779b41b50e95c642778b7660afb3c9
RitikDwivedi <ritikcu@gmail.com>
2026-07-20 16:16:04 +0530

    UPDATED

--- my_model2/myapp/urls.py
+++ my_model2/myapp/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.contact_form, name="contact_form"),
+    path('submit_form/', views.submit_form, name="submit_form"),
 ]
+
+# https://github.com/ritik1172/Django-12313/tree/main/my_model2
