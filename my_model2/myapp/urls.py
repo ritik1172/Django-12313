46c6723d6127365de29d1b52e8cc9fd6918569d4
RitikDwivedi <ritikcu@gmail.com>
2026-07-20 11:34:52 +0530

    db

--- my_model2/myapp/urls.py
+++ my_model2/myapp/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.contact_form, name="contact_form"),
+    path('submit_form/', views.submit_form, name="submit_form"),
 ]
--- my_model2/myapp/views.py
+++ my_model2/myapp/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse;
 from .models import Student
 
 # Create your views here.
-def home(request):
-    students = Student.objects.all();
-    return render(request, "home.html",{"students": students} );
+def contact_form(request):
+    return render(request, "contact.html")
+
+def submit_form(request):
+    if request.method == "POST":
+        name = request.POST.get("name");
+        email = request.POST.get("email");
+        age = request.POST.get("age");
+        
+        if name and email:
+            Student.objects.create(name=name, email=email, age=age);
+            return HttpResponse(f"thanks {name} for saving this form...");
+        else:
+            return HttpResponse("All field should be filled...")
+    return redirect('contact_form')
+        
+        
